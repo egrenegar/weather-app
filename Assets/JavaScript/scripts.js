dae3e042a9d5d5e57e29e8eb42997163b183ae55
Emily Renegar <egrenegar@gmail.com>
2020-05-12 22:51:04 -0400

    adding weather icons

--- Assets/JavaScript/scripts.js
+++ Assets/JavaScript/scripts.js
@@ -1,71 +1,74 @@
 
-
-
 var searches = JSON.parse(localStorage.getItem('searches')) || [];
+var d = new Date();
+var strDate = (d.getMonth()+1) + "/" + d.getDate() + "/" +  d.getFullYear();
 
 
 function renderSearches() {
     $('#searches').empty();
 
     for (var i = 0; i < searches.length; i++) {
-        var search = $('<a>').text(searches[i]);
-        search.attr('class', 'list-group-item list-group-item-action city');
+        var search = $('<a>').text(searches[i])
+        search.attr('class', 'list-group-item list-group-item-action city')
         search.attr('data-city', searches[i])
-        $('#searches').append(search);
-
-        // getWeather();
+        $('#searches').append(search)
     }
+
+    getWeather();
 }
 
 $('form').on('submit', function (event) {
     var city = $("#city-input").val().trim();
-    console.log(city);
-    event.preventDefault;
+    console.log(city)
+    event.preventDefault
 
     // pushes city search into searches array
-    searches.push(city);
+    searches.push(city)
 
     localStorage.setItem('searches', JSON.stringify(searches))
 
-    // clears out the search input bar
-    $('#city-input').val('');
+    // clears out the search input field
+    $('#city-input').val('')
+
     // render the previous searches to the sidebar
     renderSearches();
-
 });
 
-// when user submits a city, the weather for that city is returned
-// current weather and 5-day forecast
 function getWeather() {
     // grabs the data-city attribute from the event target
-    var city = $(this).attr('data-city');
+    var city = $(this).attr('data-city')
     var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=20a0e218c2b35d7287d3b43b10aa6e1f&units=imperial';
-    
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        console.log(response)
         var cityName = response.name
-        var temp = response.main.temp
-        var windSpeed = response.wind.speed
+        var temp = (response.main.temp).toFixed(1)
+        var windSpeed = (response.wind.speed).toFixed(1)
         var humidity = response.main.humidity
-        // var lat = response
-        // var lon = response
+        var lat = response.coord.lat
+        var lon = response.coord.lon
+        var icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
         $('#city-name').text(cityName)
-        $('#temp').text('Temperature: ' + temp + ' (F)')
-        $('#humidity').text('Humidity: ' + humidity)
+        $('#city-name').append(icon)
+        $('#temp').text('Temperature: ' + temp + ' \xB0F')
+        $('#humidity').text('Humidity: ' + humidity + '%')
         $('#wind-speed').text('Wind Speed: ' + windSpeed + ' mph')
 
-        getUVIndex(lat, lon);
-        // $('#uv-index').text('UV Index: ' + uvIndex)
+        var queryURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=20a0e218c2b35d7287d3b43b10aa6e1f&lat=' + lat + '&lon=' + lon;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            var uvIndex = response.value
+            $('#uv-index').text('UV Index: ' + uvIndex)
+        })
+        
     });
-
-  
-
-
-    // renderWeather();
 }
 
 // function renderWeather(){
