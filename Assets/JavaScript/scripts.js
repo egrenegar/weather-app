bd23684ec2a0f2b085379588f79be92b9cf5b6a3
Emily Renegar <egrenegar@gmail.com>
2020-05-13 22:55:43 -0400

    pushing final updates

--- Assets/JavaScript/scripts.js
+++ Assets/JavaScript/scripts.js
@@ -19,7 +19,7 @@ function renderSearches() {
 $('form').on('submit', function (event) {
     event.preventDefault();
 
-    var city = $("#city-input").val().trim();
+    var city = $('#city-input').val().trim();
     console.log(city)
     // pushes city search into searches array
     searches.push(city)
@@ -48,7 +48,7 @@ function getWeather(city) {
         var lat = response.coord.lat
         var lon = response.coord.lon
         var icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
-        $('#city-name').text(cityName + " " + strDate)
+        $('#city-name').text(cityName + ' ' + strDate)
         $('#city-name').append(icon)
         $('#temp').text('Temperature: ' + temp + ' \xB0F')
         $('#humidity').text('Humidity: ' + humidity + '%')
@@ -80,36 +80,41 @@ function getForecast(city) {
     }).then(function (response) {
         console.log(response);
 
-        var day1Temp = (response.list[3].main.temp).toFixed(1);
-        var day1Humid = (response.list[3].main.humidity);
-        var day2Temp = (response.list[11].main.temp).toFixed(1);
-        var day2Humid = (response.list[11].main.humidity);
-        var day3Temp = (response.list[19].main.temp).toFixed(1);
-        var day3Humid = (response.list[19].main.humidity);
-        var day4Temp = (response.list[27].main.temp).toFixed(1);
-        var day4Humid = (response.list[27].main.humidity);
-        var day5Temp = (response.list[35].main.temp).toFixed(1);
-        var day5Humid = (response.list[35].main.humidity);
-        var day1icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[3].weather[0].icon + '.png');
-        var day2icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[11].weather[0].icon + '.png');
-        var day3icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[19].weather[0].icon + '.png');
-        var day4icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[27].weather[0].icon + '.png');
-        var day5icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[35].weather[0].icon + '.png');
-        $('#day1temp').append(day1Temp + ' \xB0F');
-        $('#day1hum').append(day1Humid + '%');
-        $('#day2temp').append(day2Temp + ' \xB0F');
-        $('#day2hum').append(day2Humid + '%');
-        $('#day3temp').append(day3Temp + ' \xB0F');
-        $('#day3hum').append(day3Humid + '%');
-        $('#day4temp').append(day4Temp + ' \xB0F');
-        $('#day4hum').append(day4Humid + '%');
-        $('#day5temp').append(day5Temp + ' \xB0F');
-        $('#day5hum').append(day5Humid + '%');
-        $('#day1date').append(day1icon);
-        $('#day2date').append(day2icon);
-        $('#day3date').append(day3icon);
-        $('#day4date').append(day4icon);
-        $('#day5date').append(day5icon);
+        var day1Temp = (response.list[3].main.temp).toFixed(1)
+        var day1Humid = (response.list[3].main.humidity)
+        var day2Temp = (response.list[11].main.temp).toFixed(1)
+        var day2Humid = (response.list[11].main.humidity)
+        var day3Temp = (response.list[19].main.temp).toFixed(1)
+        var day3Humid = (response.list[19].main.humidity)
+        var day4Temp = (response.list[27].main.temp).toFixed(1)
+        var day4Humid = (response.list[27].main.humidity)
+        var day5Temp = (response.list[35].main.temp).toFixed(1)
+        var day5Humid = (response.list[35].main.humidity)
+        var day1icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[3].weather[0].icon + '.png')
+        var day2icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[11].weather[0].icon + '.png')
+        var day3icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[19].weather[0].icon + '.png')
+        var day4icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[27].weather[0].icon + '.png')
+        var day5icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[35].weather[0].icon + '.png')
+        $('#day1date').text(response.list[3].dt_txt.split(' ')[0])
+        $('#day2date').text(response.list[11].dt_txt.split(' ')[0])
+        $('#day3date').text(response.list[19].dt_txt.split(' ')[0])
+        $('#day4date').text(response.list[27].dt_txt.split(' ')[0])
+        $('#day5date').text(response.list[35].dt_txt.split(' ')[0])
+        $('#day1temp').append(day1Temp + ' \xB0F')
+        $('#day1hum').append(day1Humid + '%')
+        $('#day2temp').append(day2Temp + ' \xB0F')
+        $('#day2hum').append(day2Humid + '%')
+        $('#day3temp').append(day3Temp + ' \xB0F')
+        $('#day3hum').append(day3Humid + '%')
+        $('#day4temp').append(day4Temp + ' \xB0F')
+        $('#day4hum').append(day4Humid + '%')
+        $('#day5temp').append(day5Temp + ' \xB0F')
+        $('#day5hum').append(day5Humid + '%')
+        $('#day1date').append(day1icon)
+        $('#day2date').append(day2icon)
+        $('#day3date').append(day3icon)
+        $('#day4date').append(day4icon)
+        $('#day5date').append(day5icon)
 
     })
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # weather-dashboard
 UNCC Coding Boot Camp - Homework 06
+
+## Assignment
+* This assignment was to build a weather dashboard that runs in the browser. It features dynamically updated HTML and CSS and uses the OpenWeather API for weather and city data.
+
+## URLs
+* GitHub Repository: https://github.com/egrenegar/weather-dashboard
+* Deployed Application: https://egrenegar.github.io/weather-dashboard/
+
