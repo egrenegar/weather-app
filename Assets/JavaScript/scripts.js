310fc5ee08fa8a92ce51269a2742fd84c86a9a7f
Emily Renegar <egrenegar@gmail.com>
2020-05-13 22:38:33 -0400

    adding changes to dates and updating 5-day forecast

--- Assets/JavaScript/scripts.js
+++ Assets/JavaScript/scripts.js
@@ -13,6 +13,7 @@ function renderSearches() {
         search.attr('data-city', searches[i])
         $('#searches').append(search)
     }
+
 }
 
 $('form').on('submit', function (event) {
@@ -47,7 +48,7 @@ function getWeather(city) {
         var lat = response.coord.lat
         var lon = response.coord.lon
         var icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
-        $('#city-name').text(cityName)
+        $('#city-name').text(cityName + " " + strDate)
         $('#city-name').append(icon)
         $('#temp').text('Temperature: ' + temp + ' \xB0F')
         $('#humidity').text('Humidity: ' + humidity + '%')
@@ -64,16 +65,53 @@ function getWeather(city) {
             $('#uv-index').text('UV Index: ' + uvIndex)
         })
 
-        var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=20a0e218c2b35d7287d3b43b10aa6e1f'
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response)
-        })
-
     });
+
+    getForecast(city);
+}
+
+function getForecast(city) {
+
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=20a0e218c2b35d7287d3b43b10aa6e1f&units=imperial'
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var day1Temp = (response.list[3].main.temp).toFixed(1);
+        var day1Humid = (response.list[3].main.humidity);
+        var day2Temp = (response.list[11].main.temp).toFixed(1);
+        var day2Humid = (response.list[11].main.humidity);
+        var day3Temp = (response.list[19].main.temp).toFixed(1);
+        var day3Humid = (response.list[19].main.humidity);
+        var day4Temp = (response.list[27].main.temp).toFixed(1);
+        var day4Humid = (response.list[27].main.humidity);
+        var day5Temp = (response.list[35].main.temp).toFixed(1);
+        var day5Humid = (response.list[35].main.humidity);
+        var day1icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[3].weather[0].icon + '.png');
+        var day2icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[11].weather[0].icon + '.png');
+        var day3icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[19].weather[0].icon + '.png');
+        var day4icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[27].weather[0].icon + '.png');
+        var day5icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + response.list[35].weather[0].icon + '.png');
+        $('#day1temp').append(day1Temp + ' \xB0F');
+        $('#day1hum').append(day1Humid + '%');
+        $('#day2temp').append(day2Temp + ' \xB0F');
+        $('#day2hum').append(day2Humid + '%');
+        $('#day3temp').append(day3Temp + ' \xB0F');
+        $('#day3hum').append(day3Humid + '%');
+        $('#day4temp').append(day4Temp + ' \xB0F');
+        $('#day4hum').append(day4Humid + '%');
+        $('#day5temp').append(day5Temp + ' \xB0F');
+        $('#day5hum').append(day5Humid + '%');
+        $('#day1date').append(day1icon);
+        $('#day2date').append(day2icon);
+        $('#day3date').append(day3icon);
+        $('#day4date').append(day4icon);
+        $('#day5date').append(day5icon);
+
+    })
 
     renderSearches();
 }
