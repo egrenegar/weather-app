60dd36cfbd69b6a00546a1d460fa49415905afc4
Emily Renegar <egrenegar@gmail.com>
2020-05-14 20:48:59 -0400

    adding some comments

--- Assets/JavaScript/scripts.js
+++ Assets/JavaScript/scripts.js
@@ -13,12 +13,11 @@ function renderSearches() {
         search.attr('data-city', searches[i])
         $('#searches').append(search)
     }
-
 }
 
 $('form').on('submit', function (event) {
     event.preventDefault();
-
+    // get the user's search from input field
     var city = $('#city-input').val().trim();
     console.log(city)
     // pushes city search into searches array
@@ -27,7 +26,7 @@ $('form').on('submit', function (event) {
     localStorage.setItem('searches', JSON.stringify(searches))
     // clears out the search input field
     $('#city-input').val('')
-    // render the previous searches to the sidebar
+
     getWeather(city);
 
 });
@@ -36,6 +35,7 @@ function getWeather(city) {
     console.log(city);
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=20a0e218c2b35d7287d3b43b10aa6e1f&units=imperial';
 
+    // requesting the current weather
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -56,6 +56,7 @@ function getWeather(city) {
 
         var queryURL = 'https://api.openweathermap.org/data/2.5/uvi?appid=20a0e218c2b35d7287d3b43b10aa6e1f&lat=' + lat + '&lon=' + lon;
 
+        // requesting the UV Index
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -64,60 +65,62 @@ function getWeather(city) {
             var uvIndex = response.value
             $('#uv-index').text('UV Index: ' + uvIndex)
         })
-
     });
-
     getForecast(city);
 }
 
 function getForecast(city) {
-
     var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=20a0e218c2b35d7287d3b43b10aa6e1f&units=imperial'
 
+    // requesting the 5-day forecast
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        // getting temperature for five days
         var day1Temp = (response.list[3].main.temp).toFixed(1)
-        var day1Humid = (response.list[3].main.humidity)
         var day2Temp = (response.list[11].main.temp).toFixed(1)
-        var day2Humid = (response.list[11].main.humidity)
         var day3Temp = (response.list[19].main.temp).toFixed(1)
-        var day3Humid = (response.list[19].main.humidity)
         var day4Temp = (response.list[27].main.temp).toFixed(1)
-        var day4Humid = (response.list[27].main.humidity)
         var day5Temp = (response.list[35].main.temp).toFixed(1)
+        // getting humidity for five days
+        var day1Humid = (response.list[3].main.humidity)
+        var day2Humid = (response.list[11].main.humidity)
+        var day3Humid = (response.list[19].main.humidity)
+        var day4Humid = (response.list[27].main.humidity)
         var day5Humid = (response.list[35].main.humidity)
+        // getting weather icon for five days
         var day1icon = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + response.list[3].weather[0].icon + '.png')
         var day2icon = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + response.list[11].weather[0].icon + '.png')
         var day3icon = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + response.list[19].weather[0].icon + '.png')
         var day4icon = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + response.list[27].weather[0].icon + '.png')
         var day5icon = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + response.list[35].weather[0].icon + '.png')
+        // render date to div for each day
         $('#day1date').text(response.list[3].dt_txt.split(' ')[0])
         $('#day2date').text(response.list[11].dt_txt.split(' ')[0])
         $('#day3date').text(response.list[19].dt_txt.split(' ')[0])
         $('#day4date').text(response.list[27].dt_txt.split(' ')[0])
         $('#day5date').text(response.list[35].dt_txt.split(' ')[0])
+        // append temperature to each div
         $('#day1temp').append(day1Temp + ' \xB0F')
-        $('#day1hum').append(day1Humid + '%')
         $('#day2temp').append(day2Temp + ' \xB0F')
-        $('#day2hum').append(day2Humid + '%')
         $('#day3temp').append(day3Temp + ' \xB0F')
-        $('#day3hum').append(day3Humid + '%')
         $('#day4temp').append(day4Temp + ' \xB0F')
-        $('#day4hum').append(day4Humid + '%')
         $('#day5temp').append(day5Temp + ' \xB0F')
+        // append humidity to each div
+        $('#day1hum').append(day1Humid + '%')
+        $('#day2hum').append(day2Humid + '%')
+        $('#day3hum').append(day3Humid + '%')
+        $('#day4hum').append(day4Humid + '%')
         $('#day5hum').append(day5Humid + '%')
+        // append icon to each div
         $('#day1date').append(day1icon)
         $('#day2date').append(day2icon)
         $('#day3date').append(day3icon)
         $('#day4date').append(day4icon)
         $('#day5date').append(day5icon)
-
     })
-
     renderSearches();
 }
 
